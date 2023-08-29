d3be7e8d781e38de4b6da24691a842eeec458037
sergeevii123 <sergeevii123@gmail.com>
2023-08-29 22:53:50 +0300

    change file save

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const ipfsClient = require('ipfs-http-client');
 const uint8ArrayConcat = require("uint8arrays/concat");
 const fs = require('fs');
 const path = require('path');
-const CID = 'bafykbzaceatihez66rzmzuvfx5nqqik73hlphem3dvagmixmay3arvqd66ng6'; // Replace with your constant path/CID
+const CID = 'bafybeiagtega7rplvxmisdplxpc47oaqnkf4vwuxnk3tb6ufe2yahnvn2i/2697.png'; // Replace with your constant path/CID
+const ipfs = ipfsClient("http://gateway.ipfs.io")
+console.log("created ipfs");
 
 async function downloadIpfsFile(ipfs, cid) {
     let data = [];
@@ -20,13 +22,11 @@ async function downloadIpfsFile(ipfs, cid) {
 
 async function download(cid) {
     console.log("Downloading file:", cid);
-    const ipfs = ipfsClient("http://gateway.ipfs.io")
-    console.log("created ipfs");
     try {
         const data = await downloadIpfsFile(ipfs, cid);
-        
+        const fileName = cid.split('/').slice(-1)[0]; // Get the file name from the CID
         // Define the path where the file will be saved
-        const filePath = path.join(__dirname, 'downloaded_file'); // This will save in the current directory
+        const filePath = path.join(__dirname, fileName); // This will save in the current directory
         console.log("File path:", filePath);
         
         // Write data to a file
