5530f65b9ff23e3a8ca285529565075c7d78e070
sergeevii123 <sergeevii123@gmail.com>
2023-12-26 22:51:47 +0200

    change default nft

--- src/constants/other.ts
+++ src/constants/other.ts
@@ -1,2 +1,2 @@
 export const ERC721EnumerableInterfaceID = "0x780e9d63";
-export const sampleNFTAddress = "0xeE598F149FFFf85b60EE84f58c95c5aD6cf86B10";
+export const sampleNFTAddress = "0x08DE81C36848A59188A6A1474c8985D555bD4440";
